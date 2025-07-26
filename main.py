e350f3d0c3b38e4175599b2e7615e9f9a92235b6
Jefferson Silemen <jeffersonvieiratec@gmail.com>
2025-07-26 16:15:33 -0300

    final logic

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 def sort(width, height, length, mass) -> str:
+    package_type = []
     if width >= 150 or height >= 150 or length >= 150:
-        package_type = 'bulky'
+        package_type.append('bulky')
     elif (width * height * length) >= 1000000:
-        package_type = 'bulky'
+        package_type.append('bulky')
     elif mass >= 20:
-        package_type = 'heavy'
+        package_type.append('heavy')
+
+    if "bulky" in package_type and "heavy" in package_type:
+        deliver = "REJECTED"
+    elif "bulky" in package_type:
+        deliver = "SPECIAL"
+    elif "heavy" in package_type:
+        deliver = "SPECIAL"
     else:
-        package_type = 'normal'
-    return package_type
+        deliver = "STANDARD"
+
+    return deliver
